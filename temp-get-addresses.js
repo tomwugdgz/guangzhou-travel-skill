f1840968ba9737e603275c15a2df1fe29cca1673
Tom Wu <duckwolf@qq.com>
2026-04-25 17:25:36 +0800

    feat: 添加北京路美食10条（陶陶居/莲香楼/向群饭店/民记煲仔饭等）

---
+++ temp-get-addresses.js
@@ -0,0 +1,26 @@
+const { geocoder } = require('./src/map/tencent-map');
+
+const addresses = [
+  '广州市越秀区北京路天河城',
+  '广州市越秀区北京路313号',
+  '广州市越秀区北京路',
+  '广州市越秀区惠福东路',
+  '广州市越秀区大南路',
+  '广州市越秀区文明路'
+];
+
+(async () => {
+  for (const addr of addresses) {
+    try {
+      const result = await geocoder(addr, '广州市');
+      if (result.status === 0) {
+        console.log(`OK: ${addr} | ${result.result.location.lat},${result.result.location.lng}`);
+      } else {
+        console.log(`FAIL: ${addr} | ${result.message}`);
+      }
+    } catch (e) {
+      console.log(`ERROR: ${addr} | ${e.message}`);
+    }
+    await new Promise(r => setTimeout(r, 300));
+  }
+})();
